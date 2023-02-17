20eaea0ad2760bb85f958b114ddb29312664eb53
Maktor <66954908+Maktor@users.noreply.github.com>
2023-02-17 13:53:19 -0500

    Updated Files

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello World!')
+})
+
+app.get("*", (req, res) => {
+    res.send("<h1>404 Page</h1>")
 })
 
 app.listen(process.env.PORT)
