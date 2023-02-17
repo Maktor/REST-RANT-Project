531e19c5062ed2536221223663295576f4eb0f7a
Maktor <66954908+Maktor@users.noreply.github.com>
2023-02-17 14:01:01 -0500

    Updated Files

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -9,5 +11,3 @@ app.get('/', (req, res) => {
 app.get("*", (req, res) => {
     res.status(404).send("<h1>404 Page</h1>")
 })
-
-app.listen(process.env.PORT)
